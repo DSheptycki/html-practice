8262a84f356b737c3d614dbd55e24695f0f3e826
Danny Sheptycki <Danny98227@outlook.com>
2022-09-19 10:35:07 -0500

    Added JS

---
+++ functions.js
@@ -0,0 +1,6 @@
+
+function myFunction() {
+    document.getElementById("aboutMe").innerHTML = "ok you're right, unfortunately :("; 
+const element = document.getElementById("button");
+    element.remove();
+}
